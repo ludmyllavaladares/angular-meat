dc18c05724f4383b0cadff5f61973c72d15c925a
ludmyllavaladares <ludmylla.valadares@a.ficr.edu.br>
2021-08-17 14:20:52 -0300

    incluíndo métodos adicionar e limpar carrinho

--- src/app/restaurant-detail/shopping-cart/shopping-cart.component.ts
+++ src/app/restaurant-detail/shopping-cart/shopping-cart.component.ts
@@ -11,12 +11,23 @@ export class ShoppingCartComponent implements OnInit {
 
   constructor(private shoppingCartService: ShoppingCartService) {}
 
-
   ngOnInit() {
   }
 
   items(): any[] {
     return this.shoppingCartService.items;
+  }
+
+  clear(){
+    this.shoppingCartService.clear();
+  }
+
+  removeItem(item: any){
+    this.shoppingCartService.removeItem(item)
+  }
+
+  addItem(item: any){
+    this.shoppingCartService.addItem(item)
   }
 
   total(): number {
